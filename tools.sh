9bc15e6489f806ad6d910f8716f49b1d63e5436e
MXNT4NN4. <rique.souza06@icloud.com>
2025-03-17 17:04:01 -0300

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install -y gobuster
+
+# Gobuster é utilizado para realizar o reconhecimento de diretórios e arquivos
+
+apt install -y burpsuite
+
+# Proxy para capturar requisições Web
+
+apt install -y seclists
+
+# Wordlist completa e atualizada
