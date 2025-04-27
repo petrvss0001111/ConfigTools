04a7fc3ff9b100225fa0e30dfae980b1ef51c121
Petrvs <rique.souza06@icloud.com>
2025-04-27 16:51:01 -0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -3,11 +3,14 @@
 apt install net-tools -y
 apt install mysql postgresql -y
 
+mkdir /opt/ToolsFile
+
 # Instalação Golang
-cd /usr/local && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /root -xzf go1.22.0.linux-amd64.tar.gz
+cd /opt/ToolsFile && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /root -xzf go1.24.2.linux-amd64.tar.gz
 export PATH=$PATH:/root/go/bin
 echo "PATH=\$PATH:/root/go/bin" >> /root/.bashrc
+mkdir Packages && mv go1* /opt/Packages
 
 
 # Instalação Python3
@@ -15,7 +18,7 @@ sudo apt-get install python3.9 -y
 apt install python3-pip
 
 # Instalação de Ferramentas
-mkdir /opt/github/
-cd /opt/github/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
+mkdir /opt/ToolsFile/github/
+cd /opt/ToolsFile/github/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
 apt install -y burpsuite
 mkdir /usr/share/wordlists/Seclists && cd /usr/share/wordlists/Seclists/ && git clone https://github.com/danielmiessler/SecLists.git
