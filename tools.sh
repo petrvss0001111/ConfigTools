a569ec502145aa1e5e5518f61f323178cb07565b
Petrvs <rique.souza06@icloud.com>
2025-04-27 11:04:45 -0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-apt install -y gobuster
+# Instalação Golang
+cd /usr/local && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && rm -rf /usr/bin/go tar -C /usr/bin/ -xzf go1.24.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/bin/go/bin
 
-# Gobuster é utilizado para realizar o reconhecimento de diretórios e arquivos
+# Instalação Python3
+sudo apt-get install python3.9 -y
 
+# Instalação de Ferramentas
+mkdir /opt/gobuster/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
 apt install -y burpsuite
-
-# Proxy para capturar requisições Web
-
-apt install -y seclists
-
-# Wordlist completa e atualizada
+mkdir /usr/share/wordlists/Seclists && cd /usr/share/wordlists/Seclists/ && git clone https://github.com/danielmiessler/SecLists.git
