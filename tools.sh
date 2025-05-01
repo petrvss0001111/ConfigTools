cdf56ae8adbbf49e41530cd353d8d704074ca8ee
Petrvs <rique.souza06@icloud.com>
2025-05-01 18:09:00 -0300

    feat: directory configuration

--- tools.sh
+++ tools.sh
@@ -2,15 +2,16 @@
 
 apt install net-tools -y
 apt install mysql postgresql -y
+apt install wget curl -y
 
-mkdir /opt/ToolsFile
+mkdir /opt/Tools
 
 # Instalação Golang
-cd /opt/ToolsFile && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /root -xzf go1.24.2.linux-amd64.tar.gz
+cd /opt/Tools && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && rm -rf /root/go && tar -C /root -xzf go1.24.2.linux-amd64.tar.gz
 export PATH=$PATH:/root/go/bin
-echo "PATH=\$PATH:/root/go/bin" >> /root/.bashrc
-mkdir Packages && mv go1* /opt/Packages
+echo "PATH=\$PATH:/root/go/bin" >> /root/.bashrc && echo "PATH=\$PATH:/root/go/bin" >> /root/.zshrc
+mkdir /opt/Tools/Packages && mv go1* /opt/Tools/Packages/
 
 
 # Instalação Python3
@@ -18,7 +19,7 @@ sudo apt-get install python3.9 -y
 apt install python3-pip
 
 # Instalação de Ferramentas
-mkdir /opt/ToolsFile/github/
-cd /opt/ToolsFile/github/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
+mkdir /opt/Tools/github/
+cd /opt/Tools/github/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
 apt install -y burpsuite
 mkdir /usr/share/wordlists/Seclists && cd /usr/share/wordlists/Seclists/ && git clone https://github.com/danielmiessler/SecLists.git
