02e030966bd7abb4a3f3f61e9339a444b631af4a
Petrvsss <rique.souza06@icloud.com>
2025-05-15 12:27:54 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Essencials
+# Essencials Tools
 apt install wget curl -y
 apt install git make gcc -y
 apt install net-tools -y
 apt install libpcap-dev -y
 apt install nc -y
 
-# Instalação Ferramentas Essenciais II
+# Essencials Tools II
 apt install mysql postgresql openssh-client -y
 apt install tcpdump -y
 apt install openssh -y
 
-mkdir /opt/Tools
+mkdir ~/Config && cd ~/Config
 
 # Instalação Golang
-cd /opt/Tools && wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && rm -rf /root/go && tar -C /root -xzf go1.24.2.linux-amd64.tar.gz
 export PATH=$PATH:/root/go/bin
 echo "PATH=\$PATH:/root/go/bin" >> /root/.bashrc && echo "PATH=\$PATH:/root/go/bin" >> /root/.zshrc
-mkdir /opt/Tools/Packages && mv go1* /opt/Tools/Packages/
+mkdir ~/Config/Packages && mv go1* ~/Config/Packages/
 
 
 # Instalação Python3
@@ -27,14 +27,14 @@ sudo apt-get install python3.9 -y
 apt install python3-pip
 
 # Instalação de Ferramentas Para Pentest
-mkdir /opt/Tools/github/
-cd /opt/Tools/github/ && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
+mkdir ~/Config/Github
+cd  ~/Config/Github && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
 apt install -y burpsuite
-mkdir /usr/share/wordlists/Seclists && cd /usr/share/wordlists/Seclists/ && git clone https://github.com/danielmiessler/SecLists.git
+mkdir /usr/share/wordlists/ && cd /usr/share/wordlists/ && git clone https://github.com/danielmiessler/SecLists.git
 
 
 # Instalação de Ferramentas Para Windows
-mkdir /opt/Tools/WindowsResoures
-cd /opt/Tools/WindowsResources
+mkdir  ~/Config/WindowsResoures
+cd  ~/Config/WindowsResources
 git clone https://github.com/bitsadmin/wesng.git
 git clone https://gitlab.com/kalilinux/packages/windows-binaries.git && cd windows-binaries && rm -rf debian && wget https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe
