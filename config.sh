b7e1685b0beba42d4b1d2f1fd8859fb5fdddac92
Petrvs <rique.souza06@icloud.com>
2025-07-10 14:26:06 -0300

    feat: nmap

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Essencials Tools
-apt install wget curl nc git make gcc net-tools libpcap-dev -y
+apt install wget curl nc git make gcc net-tools libpcap-dev openvpn -y
 
 # Essencials Tools II
 apt install mysql-common postgresql openssh-client tcpdump ssh -y
@@ -20,8 +20,9 @@ mkdir ~/Config/Packages && mv go1* ~/Config/Packages/
 sudo apt-get install python3.9 python3-pip -y
 
 # Install Github Tools for Pentest
-mkdir ~/Config/Github
-cd  ~/Config/Github && git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
+mkdir ~/Config/Github && cd ~/Config/Github
+git clone https://github.com/nmap/nmap.git
+git clone https://github.com/OJ/gobuster.git && cd gobuster && go get && go build && go install
 apt install burpsuite -y
 mkdir /usr/share/wordlists/ && cd /usr/share/wordlists/ && git clone https://github.com/danielmiessler/SecLists.git
 
